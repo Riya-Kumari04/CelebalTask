1a708e9f16e2a6b46875bfcdaebb7fd31243ba7c
Riya04_ <92142616+Riya-Kumari04@users.noreply.github.com>
2024-06-14 00:32:35 +0530

    Update Getpid.c

--- Getpid.c
+++ Getpid.c
@@ -1,22 +1,3 @@
-#include<stdio.h>
-#include<unistd.h>
-
-int main(){
-
-	int val =fork();
-	if(val==-1){
-		printf("ERROR: Process can be created\n");	
-	}
-	
-	else{
-		if(val>1){
-			printf("Parent: PID= %d, Child: PPID= %d\n",getpid(),getppid());
-			sleep(10);		
-		}
-		else{
-			printf("Parent: PID= %d, Child: PPID= %d\n",getpid(),getppid());	
-			sleep(10);	
-		}	
-	}
-	return 0;
-}
+Line 1
+Feature branch line
+Line 3
